f90572b05baf32ea351f0c7454e1852803fc80a8
adamjw3 <367147+adamjw3@users.noreply.github.com>
2024-06-06 16:19:37 +0100

    UIMS-67 add tests

--- test/controllers/AuthorisationsControllerSpec.scala
+++ test/controllers/AuthorisationsControllerSpec.scala
@@ -14,63 +14,67 @@
  * limitations under the License.
  */
 
-package controllers
-
-/*
- * Copyright 2024 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import base.TestCommonGenerators
-import models.AuthorisationRequest
-import org.scalatestplus.play._
-import org.scalatestplus.play.guice._
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
-import play.api.mvc._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar
+import org.mockito.Mockito._
+import org.mockito.ArgumentMatchers.any
+import play.api.mvc.{ControllerComponents, Results}
 import play.api.test.Helpers._
-import play.api.test._
+import play.api.test.{FakeRequest, Helpers}
+import uk.gov.hmrc.auth.core._
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import uk.gov.hmrc.ukimauthcheckerapi.controllers.AuthorisationsController
-
-import java.time.LocalDate
+import models.{AuthorisationRequest, Eori}
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class AuthorisationsControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with TestCommonGenerators with ScalaCheckPropertyChecks {
-  val controller = new AuthorisationsController(stubControllerComponents())
+class AuthorisationsControllerSpec extends AnyWordSpec with Matchers with MockitoSugar with Results {
+
+  trait Setup {
+    val mockAuthConnector: AuthConnector = mock[AuthConnector]
+    val controllerComponents: ControllerComponents = Helpers.stubControllerComponents()
+    val controller = new AuthorisationsController(controllerComponents, mockAuthConnector)
+    
+  }
 
   "AuthorisationsController" should {
 
-    "return 200 OK with empty body for valid JSON request with date populated" in {
-      forAll { authRequest: AuthorisationRequest =>
-        val authRequestWithDate = authRequest.copy(date = Some(LocalDate.now()))
-        val request = FakeRequest().withBody(authRequestWithDate)
-        val result: Future[Result] = controller.authorisations(request)
-        status(result) mustBe OK
-        contentAsString(result) mustBe empty
-      }
+    "return OK when user is authorised" in new Setup {
+      val enrolments = Enrolments(Set(Enrolment("enrolmentKey")))
+      when(mockAuthConnector.authorise(any(), any())(any(), any()))
+        .thenReturn(Future.successful(enrolments))
+
+      val request = FakeRequest().withBody(AuthorisationRequest(Seq(Eori("test-eori")), None))
+
+      val result = controller.authorisations()(request)
+
+      status(result) shouldBe OK
     }
 
-    "return 200 OK when no date is provided with empty body for valid JSON request" in {
-      forAll { authRequest: AuthorisationRequest =>
-        val authRequestWithoutDate = authRequest.copy(date = None)
-        val request = FakeRequest().withBody(authRequestWithoutDate)
-        val result: Future[Result] = controller.authorisations(request)
+    "return Unauthorized when there is no active session" in new Setup {
+      when(mockAuthConnector.authorise(any(), any())(any(), any()))
+        .thenReturn(Future.failed(new NoActiveSession("No active session") {}))
 
-        status(result) mustBe OK
-        contentAsString(result) mustBe empty
-      }
+      val request = FakeRequest().withBody(AuthorisationRequest(Seq(Eori("test-eori")), None))
+
+      val result = controller.authorisations()(request)
+
+      status(result) shouldBe UNAUTHORIZED
+      contentAsString(result) shouldBe "No active session"
     }
 
+    "return Forbidden when user is not authorised" in new Setup {
+      when(mockAuthConnector.authorise(any(), any())(any(), any()))
+        .thenReturn(Future.failed(new AuthorisationException("Forbidden") {}))
+
+      val request = FakeRequest().withBody(AuthorisationRequest(Seq(Eori("test-eori")), None))
+
+      val result = controller.authorisations()(request)
+
+      status(result) shouldBe FORBIDDEN
+      contentAsString(result) shouldBe "You are not authorized to access this resource"
+    }
   }
 }
-
